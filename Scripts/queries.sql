b4469eb9da2640b0ad1b2eabd1456190ac8352e9
mcworkaholic <wevans302@gmail.com>
2024-03-07 02:04:34 -0600

    + minor changes

--- Scripts/queries.sql
+++ Scripts/queries.sql
@@ -2,20 +2,7 @@
 
 -- This query returns professional titles from the responses and the quantity of occurrences
 -- How similar are the professional titles? Can we group some together?
-SELECT title, COUNT(title) AS title_count
-FROM responses
-GROUP BY title
+SELECT formatted_title, COUNT(formatted_title) AS title_count
+FROM formatted_responses
+GROUP BY formatted_title
 ORDER BY title_count DESC;
-
-
--- For comparison between the professional titles that were supplied and the ones that were formatted
-SELECT 
-    o.id, 
-    o.title AS original_title, 
-    f.formatted_title
-FROM 
-    original_responses o
-INNER JOIN 
-    formatted_responses f ON o.id = f.id
-ORDER BY 
-    o.id ASC;
