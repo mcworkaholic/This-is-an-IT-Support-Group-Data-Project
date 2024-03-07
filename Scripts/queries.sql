6f278478884dc7e963af47b4604d4413ff308f2c
mcworkaholic <wevans302@gmail.com>
2024-03-07 01:37:02 -0600

    + comparison query

--- Scripts/queries.sql
+++ Scripts/queries.sql
@@ -1,7 +1,21 @@
 
 
--- This query returns unique professional titles from the responses and the quantity of occurrences
+-- This query returns professional titles from the responses and the quantity of occurrences
+-- How similar are the professional titles? Can we group some together?
 SELECT title, COUNT(title) AS title_count
 FROM responses
 GROUP BY title
 ORDER BY title_count DESC;
+
+
+-- For comparison between the professional titles that were supplied and the ones that were formatted
+SELECT 
+    o.id, 
+    o.title AS original_title, 
+    f.formatted_title
+FROM 
+    original_responses o
+INNER JOIN 
+    formatted_responses f ON o.id = f.id
+ORDER BY 
+    o.id ASC;
