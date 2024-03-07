4d4337169cb3e3818b0fa938bbc2ebc2a2e18736
mcworkaholic <wevans302@gmail.com>
2024-03-07 01:38:46 -0600

    + formatting function, changed the reverse geocoding function, added db functionality

--- data_norming.py
+++ data_norming.py
@@ -1,98 +1,13 @@
 import json
 import re
-import googlemaps
-from dotenv import load_dotenv
 import os
 import sqlite3
+import requests
+import time
+from config import us_states, countries, patterns
 
-# Load environment variables from .env file
-load_dotenv()
-
-# Accessing the API key
-google_maps_api_key = os.getenv('GOOGLE_MAPS_API_KEY')
-
-us_states = {
-    'AL': 'Alabama',
-    'AK': 'Alaska',
-    'AZ': 'Arizona',
-    'AR': 'Arkansas',
-    'CA': 'California',
-    'CO': 'Colorado',
-    'CT': 'Connecticut',
-    'DE': 'Delaware',
-    'FL': 'Florida',
-    'GA': 'Georgia',
-    'HI': 'Hawaii',
-    'ID': 'Idaho',
-    'IL': 'Illinois',
-    'IN': 'Indiana',
-    'IA': 'Iowa',
-    'KS': 'Kansas',
-    'KY': 'Kentucky',
-    'LA': 'Louisiana',
-    'ME': 'Maine',
-    'MD': 'Maryland',
-    'MA': 'Massachusetts',
-    'MI': 'Michigan',
-    'MN': 'Minnesota',
-    'MS': 'Mississippi',
-    'MO': 'Missouri',
-    'MT': 'Montana',
-    'NE': 'Nebraska',
-    'NV': 'Nevada',
-    'NH': 'New Hampshire',
-    'NJ': 'New Jersey',
-    'NM': 'New Mexico',
-    'NY': 'New York',
-    'NC': 'North Carolina',
-    'ND': 'North Dakota',
-    'OH': 'Ohio',
-    'OK': 'Oklahoma',
-    'OR': 'Oregon',
-    'PA': 'Pennsylvania',
-    'PR': 'Puerto Rico',
-    'RI': 'Rhode Island',
-    'SC': 'South Carolina',
-    'SD': 'South Dakota',
-    'TN': 'Tennessee',
-    'TX': 'Texas',
-    'UT': 'Utah',
-    'VT': 'Vermont',
-    'VA': 'Virginia',
-    'WA': 'Washington',
-    'WV': 'West Virginia',
-    'WI': 'Wisconsin',
-    'WY': 'Wyoming'
-}
-
-
-
-countries = [
-    "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", "Australia",
-    "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin",
-    "Bhutan", "Bolivia","Bosnia", "Bosnia and Herzegovina", "Botswana", "Brazil","British Columbia","BC", "Brunei", "Bulgaria", "Burkina Faso", "Burundi",
-    "Cabo Verde", "Cambodia", "Cameroon", "Canada", "Central African Republic", "Chad", "Chile", "China", "Colombia",
-    "Comoros", "Congo", "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czechia",
-    "Democratic Republic of the Congo", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt",
-    "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Eswatini", "Ethiopia", "Fiji", "Finland",
-    "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea",
-    "Guinea-Bissau", "Guyana", "Haiti", "Holy See", "Honduras", "Hungary", "Iceland", "India", "Indonesia",
-    "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya",
-    "Kiribati", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya",
-    "Liechtenstein", "Lithuania", "Luxembourg", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali",
-    "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco",
-    "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru",
-    "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Korea", "North Macedonia",
-    "Norway", "Oman", "Pakistan", "Palau", "Palestine State", "Panama", "Papua New Guinea", "Paraguay",
-    "Peru", "Philippines", "Poland", "Portugal", "Qatar", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis",
-    "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia",
-    "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands",
-    "Somalia", "South Africa", "South Korea", "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname",
-    "Sweden", "Switzerland", "Syria", "Tajikistan", "Tanzania", "Thailand", "Timor-Leste", "Togo",
-    "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda",
-    "Ukraine", "United Arab Emirates", "United Kingdom","UK", "United States of America", "Uruguay",
-    "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe"
-]
+# Keep track of the last request time
+last_request_time = None
 
 data_directory = "Data"
 
@@ -182,6 +97,45 @@ def get_country_from_location(location, countries, us_states):
         return "United States"
     return "Unknown"
 
+def format_professional_title(title):
+    # Capitalize occurrences of " it " and " i.t. " surrounded by spaces
+    title = re.sub(r'\b(it)\b', 'IT', title, flags=re.IGNORECASE)
+    title = re.sub(r'\b(i\.t\.)\b', 'I.T.', title, flags=re.IGNORECASE)
+
+    extracted_numbers = []
+
+    for pattern, replacement in patterns.items():
+        # Check start
+        if title.startswith(pattern.strip()):
+            extracted_numbers.append(replacement.strip())
+            title = title.replace(pattern, " ", 1)
+        # Check end
+        elif title.endswith(pattern.strip()):
+            extracted_numbers.append(replacement.strip())
+            title = title[:title.rfind(pattern)] + title[title.rfind(pattern):].replace(pattern, " ", 1)
+
+        # Check middle occurrences with space padding
+        padded_pattern = " " + pattern.strip() + " "
+        if padded_pattern in title:
+            extracted_numbers.append(replacement.strip())
+            title = title.replace(padded_pattern, " ", 1)
+
+        # Remove additional occurrences carefully
+        while pattern in title:
+            if pattern.strip() in title:
+                extracted_numbers.append(replacement.strip())
+                title = title.replace(pattern, " ", 1)
+            else:
+                break  # Avoid infinite loop if no exact match is found
+
+    # Clean up any extra spaces and append extracted numbers not previously in the title
+    title = " ".join(title.split())  # Removes extra spaces
+    if extracted_numbers:
+        # Append extracted numbers, ensuring no duplicates and maintaining order
+        title += " " + " ".join(sorted(set(extracted_numbers), key=extracted_numbers.index))
+
+    return title.strip()
+
 def reformat_data(data, countries, us_states):
     reformatted_data = []
     for item in data:
@@ -191,6 +145,8 @@ def reformat_data(data, countries, us_states):
         if country != "United States":
             city = original_location.replace(country, "").strip()
             state = "N/A"  # No state for non-U.S. locations
+        elif country == "UK":
+            country = "United Kingdom"
         else:
             city, state, _ = format_us_location(original_location, us_states)
 
@@ -207,8 +163,11 @@ def reformat_data(data, countries, us_states):
         # Clean 'pay' to only include numerical values and remove "$"
         pay = re.sub(r'[^\d.]', '', pay)
 
+        # Format the title
+        formatted_title = format_professional_title(item['title'].title())
+
         reformatted_item = {
-            "title": item['title'].title(),
+            "title": formatted_title,
             "city": city,
             "state": state,
             "country/region": country if country else "Unknown",
@@ -220,28 +179,50 @@ def reformat_data(data, countries, us_states):
         reformatted_data.append(reformatted_item)
     return reformatted_data
 
-def guess_location_details(json_object, google_maps_api_key):
+def guess_location_details(json_object):
+    global last_request_time
+    
+    # Nominatim usage policy suggests at least 1 second between requests
+    request_interval = 1  # seconds
+
     # Check if country is United States and state or city is empty
     if json_object["country/region"] == "United States" and (not json_object["state"] or not json_object["city"]):
-        gmaps = googlemaps.Client(key=google_maps_api_key)
         lat, lon = json_object["lat"], json_object["lon"]
         
-        # Reverse Geocoding using Google Maps API
-        reverse_geocode_result = gmaps.reverse_geocode((lat, lon))
+        # Enforce rate limiting
+        if last_request_time is not None:
+            elapsed_time = time.time() - last_request_time
+            if elapsed_time < request_interval:
+                time.sleep(request_interval - elapsed_time)
         
-        for component in reverse_geocode_result[0]['address_components']:
-            if "administrative_area_level_1" in component["types"] and not json_object["state"]:
-                json_object["state"] = component["long_name"]
-            elif "locality" in component["types"] and not json_object["city"]:
-                json_object["city"] = component["long_name"]
-    
+        # Update the last request time
+        last_request_time = time.time()
+
+        # Construct the URL for the Nominatim API reverse geocoding request
+        url = f"https://nominatim.openstreetmap.org/reverse?lat={lat}&lon={lon}&format=json"
+        
+        # Make the request to the Nominatim API
+        response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+        data = response.json()
+        
+        # Extract state and city information from the response
+        address = data.get('address', {})
+        state = address.get('state')
+        city = address.get('city', address.get('town', address.get('village')))
+        
+        # Update the JSON object with the state and city if they were found
+        if state and not json_object["state"]:
+            json_object["state"] = state
+        if city and not json_object["city"]:
+            json_object["city"] = city
+
     return json_object
 
 # Integration into the data processing flow
-def process_data_with_guessing(data, google_maps_api_key):
+def process_data_with_guessing(data):
     reformatted_data = reformat_data(data, countries, us_states) 
     for item in reformatted_data:
-        item = guess_location_details(item, google_maps_api_key)
+        item = guess_location_details(item)  
     
     return reformatted_data
 
@@ -253,13 +234,40 @@ def process_data_with_guessing(data, google_maps_api_key):
 file_path_output = os.path.join(os.path.dirname(__file__), data_directory, modified_json_filename)
 
 # Proceed to write the processed data
-processed_data = process_data_with_guessing(data, google_maps_api_key)
+processed_data = process_data_with_guessing(data)
 with open(file_path_output, 'w') as file:
     json.dump(processed_data, file, indent=4)
 
 ################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################
 
 # FINAL STEP, convert JSON to Sqlite for further analysis
+def create_tables(conn):
+    c = conn.cursor()
+    ## FOR TESTING
+    # # Create table for original titles
+    # c.execute('''CREATE TABLE IF NOT EXISTS original_responses (
+    #              id INTEGER PRIMARY KEY AUTOINCREMENT,
+    #              title TEXT, 
+    #              city TEXT, 
+    #              state TEXT, 
+    #              country_region TEXT, 
+    #              lat REAL, 
+    #              lon REAL, 
+    #              pay REAL, 
+    #              pay_type TEXT)''')
+
+    # Create table for formatted titles
+    c.execute('''CREATE TABLE IF NOT EXISTS formatted_responses (
+                 id INTEGER PRIMARY KEY AUTOINCREMENT,
+                 formatted_title TEXT, 
+                 city TEXT, 
+                 state TEXT, 
+                 country_region TEXT, 
+                 lat REAL, 
+                 lon REAL, 
+                 pay REAL, 
+                 pay_type TEXT)''')
+    conn.commit()
 
 def insert_into_database(data, db_name="survey_responses.db"):
     # Build the database path relative to the current file and the 'Data' directory
@@ -268,21 +276,33 @@ def insert_into_database(data, db_name="survey_responses.db"):
 
     # Connect to the SQLite database at the specified path
     conn = sqlite3.connect(db_path)
+
+    # Create the tables
+    create_tables(conn)
+
     c = conn.cursor()
+    ## FOR TESTING
+    # # Prepare insert statement for original titles
+    # insert_stmt_original = '''INSERT INTO original_responses (title, city, state, country_region, lat, lon, pay, pay_type) 
+    #                           VALUES (?, ?, ?, ?, ?, ?, ?, ?)'''
 
-    # Create table with appropriate data types
-    c.execute('''CREATE TABLE IF NOT EXISTS responses
-                 (title TEXT, city TEXT, state TEXT, country_region TEXT, lat REAL, lon REAL, pay REAL, pay_type TEXT)''')
+    # Prepare insert statement for formatted titles
+    insert_stmt_formatted = '''INSERT INTO formatted_responses (formatted_title, city, state, country_region, lat, lon, pay, pay_type) 
+                               VALUES (?, ?, ?, ?, ?, ?, ?, ?)'''
 
-    # Prepare insert statement, excluding 'original response' key
-    insert_stmt = '''INSERT INTO responses (title, city, state, country_region, lat, lon, pay, pay_type) 
-                     VALUES (?, ?, ?, ?, ?, ?, ?, ?)'''
-
-    # Insert each record into the database
+    # Insert each record into the respective database table
     for item in data:
-        values = (item['title'], item['city'], item['state'], item['country/region'], 
-                  item['lat'], item['lon'], item['pay'], item['pay type'])
-        c.execute(insert_stmt, values)
+        ## FOR TESTING
+        # # Insert original title record
+        # values_original = (item['title'], item['city'], item['state'], item['country/region'], 
+        #                    item['lat'], item['lon'], item['pay'], item['pay type'])
+        # c.execute(insert_stmt_original, values_original)
+
+        # Format the title and insert formatted title record
+        formatted_title = format_professional_title(item['title'].title())
+        values_formatted = (formatted_title, item['city'], item['state'], item['country/region'], 
+                            item['lat'], item['lon'], item['pay'], item['pay type'])
+        c.execute(insert_stmt_formatted, values_formatted)
 
     # Commit the transaction and close the connection
     conn.commit()
