3e69f14397c5c9176512042d7ea262b3a1cf36ce
mcworkaholic <wevans302@gmail.com>
2024-03-07 17:46:09 -0600

    moved

--- Scripts/queries.sql
+++
@@ -1,8 +0,0 @@
-
-
--- This query returns professional titles from the responses and the quantity of occurrences
--- How similar are the professional titles? Can we group some together?
-SELECT formatted_title, COUNT(formatted_title) AS title_count
-FROM formatted_responses
-GROUP BY formatted_title
-ORDER BY title_count DESC;
